c37d92b8ba63f97e37d88dd574108d98217eb58c
pipeone97 <paulruess98@gmail.com>
2022-03-30 21:16:08 +0200

    added vertical and horizontal mirroring (not working properly yet)

--- a1/a1.py
+++ a1/a1.py
@@ -1,3 +1,5 @@
+from math import floor
+
 import numpy
 import skimage.io as skm
 import matplotlib.pyplot as plt
@@ -28,9 +30,34 @@ def mirror_image(ver_or_hor):
     height = len(image)
     width = len(image[0])
 
+    if ver_or_hor == 'ver':
+        columnR = 0
+        for columnL in range(width - 1, floor(width/2), -1):
+            print(f"{columnL} - {columnR}")
+            for line in image:
+                tmp = line[columnR]
+                line[columnR] = line[columnL]
+                line[columnL] = tmp
+            columnR += 1
+
+    if ver_or_hor == 'hor':
+        current_low_line = 0
+        current_high_line = height - 1
+        for line in image:
+            if current_low_line >= floor(height/2):
+                break
+            for idx in range(0, width - 1):
+                tmp = image[current_low_line][idx]
+                image[current_low_line][idx] = image[current_high_line][idx]
+                image[current_high_line] = tmp
+            current_low_line += 1
+            current_high_line -= 1
+    plt.imshow(image)
+    skm.show()
+
 
 if __name__ == '__main__':
-    reduce_rgb('r')
-    reduce_rgb('g')
-    reduce_rgb('b')
-    mirror_image('ver')
+    # reduce_rgb('r')
+    # reduce_rgb('g')
+    # reduce_rgb('b')
+    mirror_image('hor')
