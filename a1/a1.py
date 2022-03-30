a6884ac365970dfc44475f94f740d79ada52e3f3
exportPipe <paulruess97@gmail.com>
2022-03-30 17:26:14 +0200

    implemented rgb filtering

--- a1/a1.py
+++ a1/a1.py
@@ -3,14 +3,28 @@ import skimage.io as skm
 import matplotlib.pyplot as plt
 
 
-def show_image():
+def reduce_rgb(rgb):
     image = skm.imread(fname='images/bild01.jpg')
-    plt.imshow(image)
-    skm.show()
+
     for pixel_line in image:
         for pixel in pixel_line:
-            print(pixel)
+            if rgb == 'r':
+                pixel[1] = 0
+                pixel[2] = 0
+
+            if rgb == 'g':
+                pixel[0] = 0
+                pixel[2] = 0
+
+            if rgb == 'b':
+                pixel[0] = 0
+                pixel[1] = 0
+
+    plt.imshow(image)
+    skm.show()
 
 
 if __name__ == '__main__':
-    show_image()
+    reduce_rgb('r')
+    reduce_rgb('g')
+    reduce_rgb('b')
