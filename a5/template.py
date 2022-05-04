c9817b7cc7a48f795b978ddc7b8e542b16947941
exportPipe <paulruess97@gmail.com>
2022-05-04 13:41:24 +0200

    linear hough transformation working

--- a5/template.py
+++ a5/template.py
@@ -1,6 +1,7 @@
 import math
 from math import floor
 
+import skimage.util
 from skimage import io
 import numpy as np
 import matplotlib.pyplot as plt
@@ -105,10 +106,19 @@ def rgb2gray(rgb):
     return gray
 
 
+def invert_gray(image):
+    w, h = image.shape
+    copy = image.copy()
+    for v in range(0, w):
+        for u in range(0, h):
+            copy[u][v] = 255 - image[u][v]
+    return copy
+
+
 def linear_ht(im_edge, angle_steps: int, radius_steps: int):
     width, height = im_edge.shape
-    x_ctr = floor(width / 2)
-    y_ctr = floor(height / 2)
+    x_ctr = round(width / 2)
+    y_ctr = round(height / 2)
     n_ang = angle_steps
     d_ang = math.pi / angle_steps
     n_rad = radius_steps
@@ -131,9 +141,18 @@ def linear_ht(im_edge, angle_steps: int, radius_steps: int):
                 (x * math.cos(theta) + y * math.sin(theta)) / d_rad) + n_rad / 2
             r = round(r)
             if 0 <= r < n_rad:
-                hough_array[a][r] = hough_array[a][r] + 1
+                hough_array[a][r] += 1
 
     fill_hough_accumulutor()
+    return hough_array
+
+
+def threshold_operation(hough_array, threshold):
+    width, height = hough_array.shape
+    for v in range(0, height):
+        for u in range(0, width):
+            if hough_array[u][v] < threshold:
+                hough_array[u][v] = 0
     return hough_array
 
 
@@ -143,7 +162,9 @@ if __name__ == "__main__":
     # convert to numpy array
     img = np.array(img)
 
-    hough_arr = linear_ht(img, 100, 100)
+    hough_arr = linear_ht(img, 255, 255)
+    hough_arr_inv = invert_gray(hough_arr)
+    # hough_arr = threshold_operation(hough_arr_inv, 126)
 
     # plot img
     plt.figure(1, dpi=300)
@@ -153,6 +174,10 @@ if __name__ == "__main__":
     plt.figure(1, dpi=300)
     plt.subplot(212)
     plt.imshow(hough_arr, cmap=cm.Greys_r)
+
+    # plt.figure(1, dpi=300)
+    # plt.subplot(313)
+    # plt.imshow(hough_arr_inv, cmap=cm.Greys_r)
     plt.show()
 
     exit(0)
